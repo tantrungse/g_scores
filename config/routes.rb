8685c98e013676f81a30da426de595d951e97ee8
tantrungse <tantrungse@gmail.com>
2025-02-20 00:31:09 +0700

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  get 'pages/report'
+  get 'pages/search_score'
+  root 'pages#report'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
