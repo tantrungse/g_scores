f0893cb64aae30e8e707e2c0d62d7cee707a6f56
tantrungse <tantrungse@gmail.com>
2025-02-20 10:30:32 +0700

    add search score feature

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,9 @@ class PagesController < ApplicationController
   end
 
   def search_score
+    sbd = params[:sbd]
+    @student_score = Rails.cache.fetch("sbd_#{params[:sbd]}", expires_in: 5.minutes) do
+      StudentScore.find_by(sbd: sbd)
+    end
   end
 end
