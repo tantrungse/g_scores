d51b51434531c89a1bf6f3f8217e45e94fbe8aa3
tantrungse <tantrungse@gmail.com>
2025-02-20 20:32:15 +0700

    add feature report

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,4 @@
 //= link_tree ../images
 //= link_tree ../builds
 //= link_tree ../../../node_modules/admin-lte/dist/assets/img
+ //= link adminlte.js
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,17 @@
 class PagesController < ApplicationController
   def report
+    subjects = %i[toan ngu_van ngoai_ngu vat_li hoa_hoc sinh_hoc lich_su dia_li gdcd]
+
+    @score_distribution = {}
+
+    subjects.each do |subject|
+      @score_distribution[subject] = {
+        excellent: StudentScore.where("#{subject} >= ?", 8).count,
+        good: StudentScore.where("#{subject} >= ? AND #{subject} < ?", 6, 8).count,
+        average: StudentScore.where("#{subject} >= ? AND #{subject} < ?", 4, 6).count,
+        weak: StudentScore.where("#{subject} < ?", 4).count
+      }
+    end
   end
 
   def search_score
